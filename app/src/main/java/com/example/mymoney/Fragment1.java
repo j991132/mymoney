69186bea299fc6093d4bc1446926453cc5f35cb4
j991132 <j991132@naver.com>
2021-08-25 15:05:11 +0900

    210825 프레그먼트 임포트문제 해결

--- app/src/main/java/com/example/mymoney/Fragment1.java
+++ app/src/main/java/com/example/mymoney/Fragment1.java
@@ -1,10 +1,13 @@
 package com.example.mymoney;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+//import androidx.core.app.Fragment;
+import androidx.fragment.app.Fragment;
+
 
 
 public class Fragment1 extends Fragment {
--- app/src/main/java/com/example/mymoney/Fragment2.java
+++ app/src/main/java/com/example/mymoney/Fragment2.java
@@ -1,7 +1,8 @@
 package com.example.mymoney;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+//import androidx.core.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
