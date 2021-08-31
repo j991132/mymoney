657bae2c4f199c0efdf6a23c2b47856324bc681a
j991132 <j991132@naver.com>
2021-08-31 16:19:22 +0900

    210831 date picker 다이얼로그 띄우기

--- app/src/main/java/com/example/mymoney/Fragment1.java
+++ app/src/main/java/com/example/mymoney/Fragment1.java
@@ -1,17 +1,21 @@
 package com.example.mymoney;
 
+import android.app.DatePickerDialog;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 //import androidx.core.app.Fragment;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -24,6 +28,8 @@ public class Fragment1 extends Fragment {
     SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
     TextView nowdate;
     View view;
+    ImageButton selectdate;
+
     private String getDate() {
         mNow = System.currentTimeMillis();
         mDate = new Date(mNow);
@@ -37,6 +43,29 @@ public class Fragment1 extends Fragment {
 
         nowdate = (TextView) view.findViewById(R.id.nowdate);
         nowdate.setText(getDate());
+        selectdate =(ImageButton)view.findViewById(R.id.selectdate_btn);
+
+        Calendar c=Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+           nowdate.setText(year+"/"+(month+1)+"/"+dayOfMonth);
+            }
+        },mYear,mMonth,mDay);
+
+        selectdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectdate.isClickable()){
+                    datePickerDialog.show();
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
         return view;
     }
